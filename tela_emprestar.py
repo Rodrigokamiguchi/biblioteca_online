eaf91c3a137c91671f0a3cf436350411e2816233
hanzo-coder <rodrigokamiguchi@outlook.com>
2025-02-05 09:09:52 -0300

    foi criado a tela de emprestimo

---
+++ tela_emprestar.py
@@ -0,0 +1,39 @@
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class TelaEmprestar(Screen):
+    def __init__ (self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
+
+        self.lbl_info = Label(text="Buscar Livro")
+        layout.add_widget(self.lbl_info)
+
+        self.input_buscar = TextInput(hint_text="Digite o nome do livro ou Id")
+        layout.add_widget(self.input_buscar)
+
+        self.btn_buscar = Button(text="Buscar", on_press=self.buscar_livro)
+        layout.add_widget(self.btn_buscar)
+
+        self.lbl_detalhes = Label(text="Detalhes do Livro")
+        layout.add_widget(self.lbl_detalhes)
+
+        self.input_usuario = TextInput(hint_text="Nome do Usuário")
+        layout.add_widget(self.input_usuario)
+
+        self.btn_emprestar = Button(text="Emprestar", on_press=self.emprestar_livro)
+        layout.add_widget(self.btn_emprestar)
+
+        self.add_widget(layout)
+
+    def buscar_livro(self, instance):
+        livro_id = self.input_buscar.text
+        "Aqui vai a logica"
+
+    def emprestar_livro(self, instance):
+         usuario = self.input_usuario.text
+         livro_id = self.input_buscar.text
