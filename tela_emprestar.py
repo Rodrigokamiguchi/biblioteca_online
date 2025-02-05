b71c0af2b56cbca7c9a4dc3c6ec28eaa3c299649
hanzo-coder <rodrigokamiguchi@outlook.com>
2025-02-05 10:26:03 -0300

    adicionado botao voltar

--- tela_emprestar.py
+++ tela_emprestar.py
@@ -13,7 +13,7 @@ class TelaEmprestar(Screen):
         self.lbl_info = Label(text="Buscar Livro")
         layout.add_widget(self.lbl_info)
 
-        self.input_buscar = TextInput(hint_text="Digite o nome do livro ou Id")
+        self.input_buscar = TextInput(hint_text="Digite o nome do livro ou Id: ")
         layout.add_widget(self.input_buscar)
 
         self.btn_buscar = Button(text="Buscar", on_press=self.buscar_livro)
@@ -28,12 +28,22 @@ class TelaEmprestar(Screen):
         self.btn_emprestar = Button(text="Emprestar", on_press=self.emprestar_livro)
         layout.add_widget(self.btn_emprestar)
 
+        #Botão para volta
+        btn_voltar = Button(text="Voltar", size_hint=(1, 0.2), font_size=20, on_press=self.voltar_tela)
+        layout.add_widget(btn_voltar)
+
         self.add_widget(layout)
 
     def buscar_livro(self, instance):
         livro_id = self.input_buscar.text
         "Aqui vai a logica"
+        "Adicionar o banco de dados"
 
     def emprestar_livro(self, instance):
          usuario = self.input_usuario.text
          livro_id = self.input_buscar.text
+
+    def voltar_tela(self, instance):
+        """Retorna a tela principal"""
+
+        self.manager.current = "tela_principal"
