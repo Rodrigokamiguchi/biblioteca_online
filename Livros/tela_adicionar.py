3cf71eacc705f11e3914c9591e6e794b4e84da27
hanzo-coder <rodrigokamiguchi@outlook.com>
2025-02-07 12:05:17 -0300

    adicionado o banco de dados

--- Livros/tela_adicionar.py
+++ Livros/tela_adicionar.py
@@ -3,43 +3,70 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 from kivy.uix.textinput import TextInput
+import psycopg2
+
 
 class TelaAdicionar(Screen):
     """Tela para adicionar um livro"""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        layout = BoxLayout(orientation="vertical", padding=20, spacing=10)
+        self.layout = BoxLayout(orientation="vertical", padding=20, spacing=10)
 
         #Campo de entrada
 
-        layout.add_widget(Label(text="Titulo do livro:", font_size=20))
-        self.input_titulo = TextInput(multiline=False)
-        layout.add_widget(self.input_titulo)
+        self.input_titulo = TextInput(jint_text="Titulo do Livro")
+        self.input_autor = TextInput(jint_text="Autor")
+        self.input_ano = TextInput(jint_text="Ano de Publicação", input_filter="int")
 
-        layout.add_widget(Label(text="Autor do livro:", font_size=20))
-        self.input_autor = TextInput(multiline=False)
-        layout.add_widget(self.input_autor)
+        #Botão para adicionar livro
+        btn_adicionar    = Button(text="Adicionar Livro", on_press=self.adicionar_livro)
+        btn_voltar = Button(text="Tela Principal", on_press=self.tela_principal)
 
-        #Botão para salvar
-        btn_salvar = Button(text="salvar", size_hint=(1, 0.2), font_size=20)
-        btn_salvar.bind(on_presss=self.salvar_livro)
-        layout.add_widget(btn_salvar)
+        # Adicionar widgets do Layout
+        self.layout.add_widget(Label(text="Adicionar Novo Livro", font_size=20))
+        self.layout.add_widget(Label(self.input_titulo))
+        self.layout.add_widget(Label(self.input_ano))
+        self.layout.add_widget(Label(self.input_autor))
+        self.layout.add_widget(Label(btn_adicionar))
+        self.layout.add_widget(Label(btn_voltar))
 
-        #Botão para volta
-        btn_voltar = Button(text="Voltar", size_hint=(1, 0.2), font_size=20)
-        btn_voltar.bind(on_press=self.voltar_tela)
-        layout.add_widget(btn_voltar)
 
-        self.add_widget(layout)
+        self.add_widget(self.layout)
 
     def salvar_livro(self, instance):
-        """Função par salvar o livro no banco de daods"""
 
         titulo = self.input_titulo.text
         autor = self.input_autor.text
-        print(f"Livro '{titulo}' de {autor} salvo!")
+        ano = self.input_ano.text
+
+        if titulo and autor and ano:
+            try:
+                conn = psycopg2.connect(
+                    dbname = "biblioteca",
+                    user = "postgres",
+                    password = "123",
+                    host = "localhost",
+                    port = "5432"
+                )
+                cursor = conn.cursor()
+
+                #Inserir no banco de daods
+                cursor.execute("INSERT INTO livros (titulo, autor, ano) VALUE (%s, %s, %s)", (titulo, autor, ano))
+                conn.commit()
+
+                cursor.close()
+                conn.close()
+
+                print("Livro adicionado com sucesso!")
+                self.input_titulo.text = ""
+                self.input_autor.text = ""
+                self.input_ano.text =""
+
+            except Exception as e:
+                print("Adicionado com sucesso !", e)
+        else:
+            print("Preencha todos os campos")
 
     def voltar_tela(self, instance):
         """Retorna a tela principal"""
