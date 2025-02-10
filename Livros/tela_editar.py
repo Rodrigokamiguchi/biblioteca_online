f647c10132034fbdde0b52ef4120fce02240dc57
hanzo-coder <rodrigokamiguchi@outlook.com>
2025-02-10 09:56:19 -0300

    Finalizado

--- Livros/tela_editar.py
+++ Livros/tela_editar.py
@@ -1,3 +1,4 @@
+import psycopg2
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -7,52 +8,98 @@ from kivy.uix.boxlayout import BoxLayout
 class TelaEditar(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
+        self.layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
 
         # Campo de busca
         self.input_busca = TextInput(hint_text='Digite o título ou ID do livro')
-        layout.add_widget(self.input_busca)
-
-        self.btn_buscar = Button(text='Buscar Livro', on_press=self.buscar_livro)
-        layout.add_widget(self.btn_buscar)
+        btn_buscar = Button(text='Buscar Livro', on_press=self.buscar_livro)
 
         # Campos editáveis
         self.input_titulo = TextInput(hint_text='Título do Livro')
         self.input_autor = TextInput(hint_text='Autor')
         self.input_status = TextInput(hint_text='Status (Disponível/Emprestado)')
         
-        layout.add_widget(Label(text='Título:'))
-        layout.add_widget(self.input_titulo)
-        layout.add_widget(Label(text='Autor:'))
-        layout.add_widget(self.input_autor)
-        layout.add_widget(Label(text='Status:'))
-        layout.add_widget(self.input_status)
-        
         # Botões
-        self.btn_salvar = Button(text='Salvar Alterações', on_press=self.salvar_alteracoes)
-        self.btn_cancelar = Button(text='Cancelar', on_press=self.cancelar)
-         #Botão para volta
-        self.btn_voltar = Button(text="Voltar", size_hint=(1, 0.2), font_size=20, on_press=self.voltar_tela)
+        btn_salvar = Button(text='Salvar Alterações', on_press=self.salvar_alteracoes)
+        btn_voltar = Button(text="Voltar", size_hint=(1, 0.2), font_size=20, on_press=self.voltar_tela)
         
-        layout.add_widget(self.btn_salvar)
-        layout.add_widget(self.btn_cancelar)
-        layout.add_widget(self.btn_voltar)
+        # Adicionando widgets ao layout
+        self.layout.add_widget(Label(text="Editar Livro", font_size=20))
+        self.layout.add_widget(self.input_id)
+        self.layout.add_widget(btn_buscar)
+        self.layout.add_widget(self.input_titulo)
+        self.layout.add_widget(self.input_autor)
+        self.layout.add_widget(self.input_ano)
+        self.layout.add_widget(btn_salvar)
+        self.layout.add_widget(btn_voltar)
 
 
-        self.add_widget(layout)
+        self.add_widget(self.layout)
 
     def buscar_livro(self, instance):
-        # Aqui entraria a lógica para buscar o livro no banco de dados
-        print(f'Buscando livro: {self.input_busca.text}')
-        
+        livro_id = self.input_id.text
+
+        if livro_id:
+            try:
+                conn = psycopg2.connect(
+                    dbname = "biblioteca",
+                    user = "postgres",
+                    password = "123",
+                    host = "localhost",
+                    port = "5432"
+                )
+                cursor = conn.cursor()
+
+                cursor.execute("SELECT titulo, autor, ano, FROM livros WHERE id = %s", (livro_id,))
+                livro = cursor.fetchone()
+
+                cursor.close()
+                conn.close()
+
+                if livro:
+                    self.input_titulo.text = livro[0]
+                    self.input_autor.text = livro[1]
+                    self.input_ano.text = str(livro[2])
+                else:
+                    print("Livro não encontrado")
+            except Exception as e:
+                print("Erro ao buscar livro:", e)
+
+
     def salvar_alteracoes(self, instance):
-        # Aqui entraria a lógica para salvar as alterações no banco de dados
-        print(f'Salvando alterações para: {self.input_titulo.text}')
-        
-    def cancelar(self, instance):
-        # Voltar para a tela anterior
-        self.manager.current = 'tela_listagem'
+        livro_id = self.input_id.text
+        titulo = self.input_titulo.text
+        autor = self.input_autor.text
+        ano = self.input_ano.text
+
+        if livro_id and titulo and autor and ano:
+            try:
+                conn = psycopg2.connect(
+                    dbname = "biblioteca",
+                    user = "postgres",
+                    password = "123",
+                    host = "localhost",
+                    port = "5432"
+                )
+                cursor = conn.cursor()
+                cursor.execute(
+                    "UPDATE livros SET titulo = %s, ano = %s, WHERE id = %s",
+                    (titulo, autor, ano, livro_id)
+                )
+                conn.commit()
+                
+                cursor.close
+                conn.close()
+
+                print("Livro Autualizado com sucesso!!!")
+                self.input_id.text = ""
+                self.input_titulo.text = ""
+                self.input_autor.text = ""
+                self.input_ano.text = ""
+            except Exception as e:
+                print("Erro ao atualizar o livro: ", e)
+        else:
+            print("Preencha todos os campos!!!")
 
     def voltar_tela(self, instance):
         """Retorna a tela principal"""
--- Livros/tela_emprestar.py
+++ Livros/tela_emprestar.py
@@ -1,49 +1,115 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+import psycopg2
+from datetime import datetime, timedelta
 
-class TelaEmprestar(Screen):
-    def __init__ (self, **kwargs):
+# Conexão com o PostgreSQL
+DATABASE_CONFIG = {
+    'dbname': 'biblioteca',
+    'user': 'postegres',
+    'password': 'sua_senha',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+def conectar_bd():
+    return psycopg2.connect(**DATABASE_CONFIG)
+
+class TelaEmprestimo(Screen):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
-
-        self.lbl_info = Label(text="Buscar Livro")
-        layout.add_widget(self.lbl_info)
-
-        self.input_buscar = TextInput(hint_text="Digite o nome do livro ou Id: ")
-        layout.add_widget(self.input_buscar)
-
-        self.btn_buscar = Button(text="Buscar", on_press=self.buscar_livro)
-        layout.add_widget(self.btn_buscar)
-
-        self.lbl_detalhes = Label(text="Detalhes do Livro")
-        layout.add_widget(self.lbl_detalhes)
-
-        self.input_usuario = TextInput(hint_text="Nome do Usuário")
+        
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        
+        self.label_info = Label(text='Emprestar Livro')
+        layout.add_widget(self.label_info)
+        
+        self.input_id_livro = TextInput(hint_text='ID do Livro')
+        layout.add_widget(self.input_id_livro)
+        
+        self.input_usuario = TextInput(hint_text='Nome do Usuário')
         layout.add_widget(self.input_usuario)
-
-        self.btn_emprestar = Button(text="Emprestar", on_press=self.emprestar_livro)
+        
+        self.btn_emprestar = Button(text='Emprestar')
+        self.btn_emprestar.bind(on_press=self.emprestar_livro)
         layout.add_widget(self.btn_emprestar)
+        
+        self.btn_devolver = Button(text='Devolver')
+        self.btn_devolver.bind(on_press=self.devolver_livro)
+        layout.add_widget(self.btn_devolver)
 
-        #Botão para volta
-        btn_voltar = Button(text="Voltar", size_hint=(1, 0.2), font_size=20, on_press=self.voltar_tela)
-        layout.add_widget(btn_voltar)
-
+        self.btn_voltar = Button(text="Tela Principal", on_press=self.tela_principal)
+        layout.add_widget(self.btn_voltar)
+        
+        self.label_mensagem = Label(text='')
+        layout.add_widget(self.label_mensagem)
+        
         self.add_widget(layout)
-
-    def buscar_livro(self, instance):
-        livro_id = self.input_buscar.text
-        "Aqui vai a logica"
-        "Adicionar o banco de dados"
-
+    
     def emprestar_livro(self, instance):
-         usuario = self.input_usuario.text
-         livro_id = self.input_buscar.text
+        livro_id = self.input_id_livro.text.strip()
+        usuario = self.input_usuario.text.strip()
+        
+        if not livro_id or not usuario:
+            self.label_mensagem.text = 'Preencha todos os campos!'
+            return
+        
+        conn = conectar_bd()
+        cur = conn.cursor()
+        
+        # Verificar se o livro existe e está disponível
+        cur.execute("SELECT status FROM livros WHERE id = %s", (livro_id,))
+        resultado = cur.fetchone()
+        
+        if not resultado:
+            self.label_mensagem.text = 'Livro não encontrado!'
+        elif resultado[0] == 'Emprestado':
+            self.label_mensagem.text = 'Livro já está emprestado!'
+        else:
+            # Atualizar o status do livro e salvar o empréstimo
+            data_emprestimo = datetime.now()
+            data_devolucao = data_emprestimo + timedelta(days=14)  # 14 dias de prazo
+            cur.execute("UPDATE livros SET status = %s, usuario = %s, data_emprestimo = %s, data_devolucao = %s WHERE id = %s",
+                        ('Emprestado', usuario, data_emprestimo, data_devolucao, livro_id))
+            conn.commit()
+            self.label_mensagem.text = f'Livro emprestado! Devolução esperada: {data_devolucao.strftime("%d/%m/%Y")}.'
+        
+        cur.close()
+        conn.close()
+    
+    def devolver_livro(self, instance):
+        livro_id = self.input_id_livro.text.strip()
+        
+        if not livro_id:
+            self.label_mensagem.text = 'Informe o ID do livro!'
+            return
+        
+        conn = conectar_bd()
+        cur = conn.cursor()
+        
+        # Verificar se o livro existe e está emprestado
+        cur.execute("SELECT status FROM livros WHERE id = %s", (livro_id,))
+        resultado = cur.fetchone()
+        
+        if not resultado:
+            self.label_mensagem.text = 'Livro não encontrado!'
+        elif resultado[0] == 'Disponível':
+            self.label_mensagem.text = 'Este livro já está disponível!'
+        else:
+            # Atualizar o status para disponível e remover dados de empréstimo
+            cur.execute("UPDATE livros SET status = %s, usuario = NULL, data_emprestimo = NULL, data_devolucao = NULL WHERE id = %s",
+                        ('Disponível', livro_id))
+            conn.commit()
+            self.label_mensagem.text = 'Livro devolvido com sucesso!'
+        
+        cur.close()
+        conn.close()
 
-    def voltar_tela(self, instance):
+
+def voltar_tela(self, instance):
         """Retorna a tela principal"""
 
         self.manager.current = "tela_principal"
