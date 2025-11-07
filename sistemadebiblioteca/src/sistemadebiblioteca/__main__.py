fc3f7a00ef34dd18c5616b0670f5118f521f0b93
rodrigo <rodrigokamiguchi@gmail.com>
2025-11-07 17:41:41 -0300

    finalizado

---
+++ sistemadebiblioteca/src/sistemadebiblioteca/__main__.py
@@ -0,0 +1,4 @@
+from sistemadebiblioteca.app import main
+
+if __name__ == "__main__":
+    main()
---
+++ sistemadebiblioteca/src/sistemadebiblioteca/app.py
@@ -0,0 +1,8 @@
+"""
+Aplicativo para gerenciamento de livros e usuários em bibliotecas.
+"""
+
+
+def main():
+    # Your app logic goes here
+    print("Hello, World.")
